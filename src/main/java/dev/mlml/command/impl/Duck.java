5c93d37e6792ae8f04a462bbbfc1cdc2035958ea
Cristi <Andrei.taietu@gmail.com>
2024-08-07 19:21:38 +0300

    Fox/Duck command
    With these commands, you can generate a random picture of a fox or a duck, depending on the command you call.

---
+++ src/main/java/dev/mlml/command/impl/Duck.java
@@ -0,0 +1,51 @@
+package dev.mlml.command.impl;
+
+import dev.mlml.Utils;
+import dev.mlml.command.Command;
+import dev.mlml.command.CommandInfo;
+import dev.mlml.command.Context;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.utils.FileUpload;
+import net.dv8tion.jda.api.utils.data.DataObject;
+import net.dv8tion.jda.internal.utils.IOUtil;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Objects;
+
+@CommandInfo(
+        keywords = {"duck", "duckie"},
+        name = "Duck",
+        description = "Get a duck media",
+        permissions = {Permission.MESSAGE_SEND},
+        category = CommandInfo.Category.Fun,
+        cooldown = 5
+)
+public class Duck extends Command {
+    private static final String DUCK_API_URL = "https://random-d.uk/api/random";
+
+    @Override
+    public void execute(Context ctx) {
+        final DataObject response = Utils.sendGetRequest(DUCK_API_URL);
+
+        if (Objects.isNull(response)) {
+            ctx.fail("Failed to get dog media");
+            return;
+        }
+
+        String mediaURl = response.get("url").toString();
+
+        try {
+            URL url = new URL(mediaURl);
+            String fileName = url.getFile();
+
+            try (InputStream in = url.openStream()) {
+                byte[] fileData = IOUtil.readFully(in);
+                FileUpload fileUpload = FileUpload.fromData(fileData, fileName);
+                ctx.getMessage().replyFiles(fileUpload).queue();
+            }
+        } catch (Exception e) {
+            ctx.fail("Failed to get duck media");
+        }
+    }
+}
---
+++ src/main/java/dev/mlml/command/impl/Fox.java
@@ -0,0 +1,51 @@
+package dev.mlml.command.impl;
+
+import dev.mlml.Utils;
+import dev.mlml.command.Command;
+import dev.mlml.command.CommandInfo;
+import dev.mlml.command.Context;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.utils.FileUpload;
+import net.dv8tion.jda.api.utils.data.DataObject;
+import net.dv8tion.jda.internal.utils.IOUtil;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Objects;
+
+@CommandInfo(
+        keywords = {"fox", "foxie"},
+        name = "Fox",
+        description = "Get a fox media",
+        permissions = {Permission.MESSAGE_SEND},
+        category = CommandInfo.Category.Fun,
+        cooldown = 5
+)
+public class Fox extends Command {
+    private static final String FOX_API_URL = "https://randomfox.ca/floof/";
+
+    @Override
+    public void execute(Context ctx) {
+        final DataObject response = Utils.sendGetRequest(FOX_API_URL);
+
+        if (Objects.isNull(response)) {
+            ctx.fail("Failed to get fox media");
+            return;
+        }
+
+        String mediaURl = response.get("image").toString();
+
+        try {
+            URL url = new URL(mediaURl);
+            String fileName = url.getFile();
+
+            try (InputStream in = url.openStream()) {
+                byte[] fileData = IOUtil.readFully(in);
+                FileUpload fileUpload = FileUpload.fromData(fileData, fileName);
+                ctx.getMessage().replyFiles(fileUpload).queue();
+            }
+        } catch (Exception e) {
+            ctx.fail("Failed to get fox media");
+        }
+    }
+}
